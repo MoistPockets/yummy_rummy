a0da41c5fdfc2c427401414ed6fe543ddce6f402
Richard Mbouombouo <RichardMbouombouo@gmail.com>
2016-04-10 21:53:53 -0500

    Completed challenge

--- controller.rb
+++ controller.rb
@@ -1,12 +1,20 @@
 #Add any requires 
 	require 'sinatra'
-
+	require_relative 'models/yummy_rummy.rb'
 
 get '/' do
 	erb :index
 end
 
+get '/result/:card' do
+	@card = params[:card]
+	@result = Yummy_rummy.new.get_result
+	erb :result
+end
 
+get '/prize'  do	
+	
+end
 #insert a route that directs to /result
 	#should render the result.erb view
 
--- models/yummy_rummy.rb
+++ models/yummy_rummy.rb
@@ -1,6 +1,20 @@
 class Yummy_rummy
-
+	@@wins = 0.0
+	@@losses = 0.0
+	def initialize
+		@@games += 1
 	def get_result
-		["You Win", "You Lose"].sample
+		list = [1,2,3,4,5,6,7,8,9,10].sample
+		if list <= 9 && (@@wins/@@games <= 0.8)
+			@result = "You Win"
+			@@wins+=1
+		else
+			@result = "You Lose"
+			@@losses+=1
+		end
+
+		return @result
+		puts result
+		puts list
 	end
 end
